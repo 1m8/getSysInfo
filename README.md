9b1effbc427609921ea9c5fea246d8821983e6e1
lidalin <lidalin.se@gmail.com>
2017-12-21 13:08:32 +0800

    preg

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# getSysInfo
-根据访问者useragent获取手机型号，手机系统
+### getSysInfo
+> 根据微信访问者useragent获取手机型号，手机系统
+> 只支持微信浏览器，其他浏览器未测试
+
+***
+
+### 匹配规律
+
+meta9 : Mozilla/5.0 (Linux; Android 7.0; MHA-AL00 Build/HUAWEIMHA-AL00; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/53.0.2785.49 Mobile MQQBrowser/6.2 TBS/043307 Safari/537.36 MicroMessenger/6.5.8.1060 NetType/WIFI Language/zh_CN
+
+GALAXY S5 ：Mozilla/5.0 (Linux; Android 5.0; SM-G9009W Build/LRX21T; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/53.0.2785.49 Mobile MQQBrowser/6.2 TBS/043305 Safari/537.36 MicroMessenger/6.3.27.880 NetType/WIFI Language/zh_CN
+
+iPhone ：Mozilla/5.0 (iPhone; CPU iPhone OS 10_2_1 like Mac OS X) AppleWebKit/602.4.6 (KHTML, like Gecko) Mobile/14D27 MicroMessenger/6.5.9 NetType/WIFI Language/zh_CN
+
+规律还是比较简单，第一对括号内的内容，第一个参数区分系统，android 第三个参数是型号，iphone第二个参数是型号。
+
+
+推荐：http://www.fynas.com/ua/search?d=&b=%E5%BE%AE%E4%BF%A1&k=&page=3 useragent收集站，可以查看各种手机的useragent在各浏览器下的样式
+
+***
+
+
+### 正则匹配
+
+正则匹配百看百忘，百用百看。用到正则的以下几点：
+* 惰性匹配 只匹配第一个() 内的内容 (iPhone; CPU iPhone OS 10_2_1 like Mac OS X)
+* 断言 只匹配括号内的内容 iPhone; CPU iPhone OS 10_2_1 like Mac OS X
+
+正则：
+~~~
+
+(?<=\().*?(?=\))
+
+~~~
+
+
+
+
+
--- getSysInfo.php
+++ getSysInfo.php
@@ -1,6 +1,6 @@
 <?php 
 // +----------------------------------------------------------------------
-// | description: 根据 useragent 获取手机型号，系统
+// | description: 根据微信浏览器 useragent 获取手机型号，系统
 // +----------------------------------------------------------------------
 // | author: lidl
 // +----------------------------------------------------------------------
@@ -11,10 +11,75 @@
 
 class getSysInfo{
 	
-	private $str = 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_2_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Mobile/13D15 MicroMessenger/6.5.9 NetType/4G Language/zh_CN';
+	private $useragent;
+	
+	public function __construct(){
+		
+		$this->useragent 	=  $_SERVER['HTTP_USER_AGENT'];
+		//$this->useragent 	=  'Mozilla/5.0 (Linux; Android 6.0.1; SM-A8000 Build/MMB29M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/53.0.2785.49 Mobile MQQBrowser/6.2 TBS/043305 Safari/537.36 MicroMessenger/6.3.31.940 NetType/WIFI Language/zh_CN';
+	
+	}
+	
+	
+	public function getSys()
+	{
+		$arr = $this->preg();			
+		
+		switch (trim($arr[0]))
+		{
+			case 'iPhone':
+				return $arr[0];
+				break;
+			case 'Linux':
+				return $arr[1];
+				break;
+			default:
+				return 'Unknow';
+				break;
+				
+		}
+		
+	}
+	
+	
+	public function getBrand()
+	{
+		$arr = $this->preg();
+		
+		switch (trim($arr[0]))
+		{
+			case 'iPhone':
+				return $arr[1];
+				break;
+			case 'Linux':
+				return $arr[2];
+				break;
+			default:
+				return 'Unknow';
+				break;
+				
+		}
+		
+	}
+	
+	/**
+	 * 匹配括号内的内容
+	 * 
+	 * @return array $result 返回字符串结果，或者 0
+	 * 
+	 */
+	private function preg()
+	{
+		$result = preg_match('/(?<=\().*?(?=\))/', $this->useragent, $matches);
+		return explode(";", $matches[0]);
+		
+	}
 	
 	
 }
+
+
+
 
 
 ?>
